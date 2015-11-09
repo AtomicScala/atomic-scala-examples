71da0326e5f3b4e8a41ef095cfdc3e01aecb99dc
James Ward <james@jamesward.org>
2015-11-09 14:17:06 -0700

    use the java cp since the run is forked

--- app/utils/RunFile.scala
+++ app/utils/RunFile.scala
@@ -7,8 +7,7 @@ object RunFile {
   def apply(file: String) = {
     val settings = new GenericRunnerSettings(println _)
     settings.nc.value = true
-    settings.embeddedDefaults(getClass.getClassLoader)
-
+    settings.usejavacp.value = true
     ScriptRunner.runScript(settings, "examples/" + file, List.empty[String])
   }
 
